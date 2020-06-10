5a6ea6997b190b4e40f9d1d34ba6c6cf76f94ea8
poorvasha <poorvashadmp@gmail.com>
2020-06-10 14:16:56 +0530

    Testimonial section completed

--- js/script.js
+++ js/script.js
@@ -93,3 +93,19 @@ $(function(){
 
 });
 
+/* ===========================================
+              Testmonials
+============================================ */
+$(function(){
+  $("#testimonial-slider").owlCarousel({
+      items: 1,
+      autoplay: false,
+      smartSpeed: 700,
+      loop: true,
+      autoplayHoverPause: true,
+      nav: true,
+      dots: false,
+      navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+  });
+});
+
