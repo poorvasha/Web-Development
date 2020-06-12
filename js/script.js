157335288b28ca69e27096144af468803e22431d
poorvasha <poorvashadmp@gmail.com>
2020-06-12 13:08:14 +0530

    Client section completed

--- js/script.js
+++ js/script.js
@@ -119,3 +119,19 @@ $(function(){
   })
 });
 
+/* ===========================================
+              client
+============================================ */
+$(function(){
+  $("#clients-list").owlCarousel({
+      items: 6,
+      autoplay: false,
+      smartSpeed: 700,
+      loop: true,
+      autoplayHoverPause: true,
+      nav: true,
+      dots: false,
+      navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+  });
+});
+
